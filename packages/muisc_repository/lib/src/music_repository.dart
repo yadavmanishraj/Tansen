1f9ee5e042f97a8dbac00bba824b776799e874d8
Manish Raj Yadav <yadavmanishrajdev@gmail.com>
2023-10-22 09:19:45 +0545

    Update Music Respoitory

--- packages/muisc_repository/lib/src/music_repository.dart
+++ packages/muisc_repository/lib/src/music_repository.dart
@@ -9,4 +9,21 @@ class MusicRepository {
   Future<Map<String, List<BaseModel>>> getHome() async {
     return _jioSaavnApi.homeData();
   }
+
+  Future<List<String>> getDetailsByUrl(String permaUrl, String type) async {
+    if (type == ModelType.album) {
+      final response = await _jioSaavnApi.getAlbumDetailsByUrl(permaUrl);
+      return response.songs
+              ?.map((e) => e.veryHigh.downloadUrl ?? "")
+              .toList() ??
+          [];
+    } else if (type == ModelType.playlist) {
+      final response = await _jioSaavnApi.getPlaylistDetailsByUrl(permaUrl);
+      return response.songs
+              ?.map((e) => e.veryHigh.downloadUrl ?? "")
+              .toList() ??
+          [];
+    }
+    return [];
+  }
 }
