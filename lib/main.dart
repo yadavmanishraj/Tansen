e1262a75ad7f24d9f64d71527fd54c7c072e4aca
Manish Raj Yadav <yadavmanishrajdev@gmail.com>
2023-10-30 08:46:04 +0545

    Added bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tansen/inject.dart';
+import 'package:tansen/player_bloc_observer.dart';
 import 'package:tansen/src/features/player/bloc/music_player_bloc.dart';
 import 'package:tansen/src/routes/routes.dart';
 
 void main() async {
   setUpDependencies();
+  Bloc.observer = AppBlocObserverBlocObserver();
   runApp(const App());
 }
 
