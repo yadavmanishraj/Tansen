5dd5f1d1ca944aef82e1b26e8351a60764542b0d
Manish Raj Yadav <yadavmanishrajdev@gmail.com>
2023-11-25 00:00:44 +0545

    idk

---
+++ lib/src/features/player/services/extensions.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:muisc_repository/muisc_repository.dart';
+
+extension AddtoQueue on BaseModel {
+  void addToQueue(BuildContext context) {
+    
+  }  
+}
