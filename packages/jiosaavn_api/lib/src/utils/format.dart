3d7504a1a801e6e6313c44d05c877886ba0ddd70
yadavmanishraj <yadavmanishrajdev@gmail.com>
2024-02-13 19:00:01 +0545

    Update mapping for for HTML entity codes and Regex for matching entity regex

--- packages/jiosaavn_api/lib/src/utils/format.dart
+++ packages/jiosaavn_api/lib/src/utils/format.dart
@@ -3,12 +3,12 @@ const escapeCharacters = {
   "&amp;": "&",
   "&lt;": "<",
   "&gt;": ">",
-  "&#145;": "\\'"
+  "&#039;": "'",
 };
 
 String replaceEntity(String text) {
   return text.replaceAllMapped(
-    RegExp('&[A-Za-z]+;'),
+    RegExp('&(#[0-9]+|[a-zA-Z0-9]+);'),
     (match) {
       if (escapeCharacters.containsKey(match.group(0))) {
         return escapeCharacters[match.group(0)]!;
